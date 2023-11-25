4957ed5c7ec5533fd6c9745b2d4bfc6dff9a5170
Starinin Andrey <starininandrey@gmail.com>
2023-11-25 17:37:16 +0300

    Переименование проекта графического приложения

---
+++ PersonsDb.GuiApp/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace PersonsDb.GuiApp
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ PersonsDb.GuiApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PersonsDb.GuiApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
