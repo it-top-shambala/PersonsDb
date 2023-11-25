9d5793e5633ced9b195eefe68deb4c429f0ac073
Starinin Andrey <starininandrey@gmail.com>
2023-11-25 18:22:17 +0300

    Создание класса для команд

---
+++ PersonsDb.GuiApp/Commands/LambdaCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PersonsDb.GuiApp.Commands;
+
+public class LambdaCommand : ICommand
+{
+    private readonly Func<bool> _canExecute;
+    private readonly Action _execute;
+
+    public LambdaCommand(Action execute, Func<bool>? canExecute = null)
+    {
+        _canExecute = canExecute ?? (() => true);
+        _execute = execute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute.Invoke();
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute.Invoke();
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
