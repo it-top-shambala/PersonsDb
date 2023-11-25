cce7ba085eef5894b61efb67b6ccce254d291db4
Starinin Andrey <starininandrey@gmail.com>
2023-11-25 18:21:16 +0300

    Добавление считывания конфига

--- PersonsDb.GuiApp/App.xaml.cs
+++ PersonsDb.GuiApp/App.xaml.cs
@@ -1,8 +1,18 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
 
 namespace PersonsDb.GuiApp
 {
     public partial class App : Application
     {
+        public App()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            Resources.Add("ConnectionString", config.GetConnectionString("DefaultConnection"));
+        }
     }
 }
