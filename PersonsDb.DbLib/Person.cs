e756d5c95a3d5289d239d1d1506ed6d120118b9c
Starinin Andrey <starininandrey@gmail.com>
2023-11-25 17:35:08 +0300

    Добавление проекта подключения к БД

---
+++ PersonsDb.DbLib/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonsDb.DbLib;
+
+[Table("table_persons")]
+public class Person
+{
+    [Column("id")]
+    public int Id { get; set; }
+    
+    [Column("last_name")]
+    [Required]
+    public string LastName { get; set; }
+    
+    [Column("first_name")]
+    [Required]
+    public string FirstName { get; set; }
+    
+    [NotMapped]
+    public string FullName => $"{LastName} {FirstName}";
+}
---
+++ PersonsDb.DbLib/PersonsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonsDb.DbLib;
+
+public class PersonsDbContext : DbContext
+{
+    private readonly string _connectionString;
+    
+    public DbSet<Person> Persons { get; set; }
+
+    protected PersonsDbContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+}
